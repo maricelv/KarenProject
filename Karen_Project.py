c71dec843bdf1d67acf902905f6a763ad09c0bfa
Maricel Vicente <vicmaricel@gmail.com>
2021-05-20 12:29:11 -0400

    Updated Last push

--- Karen_Project.py
+++ Karen_Project.py
@@ -5,18 +5,20 @@ import sys
 import time
 from urllib.error import URLError
 from http.client import BadStatusLine
-import json
-import networkx as nx
-import matplotlib.pyplot as plt
+from transformers import pipeline
 
 
-
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+    
 
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                                        CONSUMER_KEY, CONSUMER_SECRET)
 
 api = twitter.Twitter(auth=auth)
-# From Cookbook
+# #Function taken from TwitterCookbook
 def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw): 
     
     # A nested helper function that handles common HTTPErrors. Return an updated
@@ -90,193 +92,7 @@ def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw):
 
 twitter_api = api
 
-# See http://bit.ly/2Gcjfzr for twitter_api.users.lookup
-
-#response = make_twitter_request(twitter_api.users.lookup, 
-                                #screen_name="marceyreads")
-
-#print(json.dumps(response, indent=1))
-# From Cookbook
-def get_friends_followers_ids(twitter_api, screen_name=None, user_id=None,
-                              friends_limit=maxint, followers_limit=maxint):
-    
-    # Must have either screen_name or user_id (logical xor)
-    assert (screen_name != None) != (user_id != None),     "Must have screen_name or user_id, but not both"
-    
-    # See http://bit.ly/2GcjKJP and http://bit.ly/2rFz90N for details
-    # on API parameters
-    
-    get_friends_ids = partial(make_twitter_request, twitter_api.friends.ids, 
-                              count=5000)
-    get_followers_ids = partial(make_twitter_request, twitter_api.followers.ids, 
-                                count=5000)
-
-    friends_ids, followers_ids = [], []
-    
-    for twitter_api_func, limit, ids, label in [
-                    [get_friends_ids, friends_limit, friends_ids, "friends"], 
-                    [get_followers_ids, followers_limit, followers_ids, "followers"]
-                ]:
-        
-        if limit == 0: continue
-        
-        cursor = -1
-        while cursor != 0:
-        
-            # Use make_twitter_request via the partially bound callable...
-            if screen_name: 
-                response = twitter_api_func(screen_name=screen_name, cursor=cursor)
-            else: # user_id
-                response = twitter_api_func(user_id=user_id, cursor=cursor)
-
-            if response is not None:
-                ids += response['ids']
-                cursor = response['next_cursor']
-        
-            print('Fetched {0} total {1} ids for {2}'.format(len(ids),                  label, (user_id or screen_name)),file=sys.stderr)
-        
-            # XXX: You may want to store data during each iteration to provide an 
-            # an additional layer of protection from exceptional circumstances
-        
-            if len(ids) >= limit or response is None:
-                break
-
-    # Do something useful with the IDs, like store them to disk...
-    return friends_ids[:friends_limit], followers_ids[:followers_limit]
-
-
-
-# Reciprocal Friends
-def reciprocal_friends(friends_ids, followers_ids):
-    set3 = set(friends_ids) 
-    set2= set(followers_ids) #find the intersection of the two
-    set1 = set3 & set2 #reciprocal_friends
-    reciprocal = list(set1)  #turns the set to a list
-    return reciprocal #returns the list of mutual friends
-
-
-# From Cookbook
-def get_user_profile(twitter_api, screen_names=None, user_ids=None):
-   
-    # Must have either screen_name or user_id (logical xor)
-    assert (screen_names != None) != (user_ids != None),     "Must have screen_names or user_ids, but not both"
-    
-    items_to_info = {}
-
-    items = screen_names or user_ids
-    
-    while len(items) > 0:
-
-        # Process 100 items at a time per the API specifications for /users/lookup.
-        # See http://bit.ly/2Gcjfzr for details.
-        
-        items_str = ','.join([str(item) for item in items[:100]])
-        items = items[100:]
-
-        if screen_names:
-            response = make_twitter_request(twitter_api.users.lookup, 
-                                            screen_name=items_str)
-        else: # user_ids
-            response = make_twitter_request(twitter_api.users.lookup, 
-                                            user_id=items_str)
-    
-        for user_info in response:
-            if screen_names:
-                items_to_info[user_info['screen_name']] = user_info
-            else: # user_ids
-                items_to_info[user_info['id']] = user_info
-
-    return items_to_info
-
-def popular(reciprocal): #Get the 5 most popular reciprocal friends
-# Find most popular Reciprocal Friend with Most Followers
-    reciprocal_followers = get_user_profile(twitter_api, user_ids=reciprocal)
-    if len(reciprocal_followers)<=5:
-        return reciprocal_followers
-# Sort list of reciprocal friends
-    dit = {id:reciprocal_followers[id]['followers_count'] for id in reciprocal_followers.keys()}
-
-    sort = sorted(dit, key = dit.get, reverse = True)
-        
-    return sort[:5] #if len(dit) > 5 else sort[:]
-
-
-
-
-# Use a crawler to get distance-2,d3,d4 friends and follow
-def crawl_followers(twitter_api, screen_name, user_id=None): #from cookbook but modified
-
-    G = nx.Graph() #graph name
-    friends_ids, followers_ids = get_friends_followers_ids(twitter_api, screen_name=screen_name, friends_limit=5000, followers_limit=5000)
-    # Find the reciprocal friends for first user
-    response = popular(reciprocal_friends(friends_ids, followers_ids))
-    print("Top 5 Reciprocal Friends is ", response)
-    #Need the first id, then the next ids runs through the loop
-    user_id = 165035772
-    for i in response:
-        G.add_edge(user_id, i)
-
-    next_queue = response
-    
-    depth = 1 #root node
-    collectedHundred = False
-    max_depth = 1000
-    # Find the reciprocal friends for the friends list
-    while depth < max_depth and not collectedHundred:
-        depth += 1
-        (queue, next_queue) = (next_queue, []) #sets the queue as the top 5 friends and next_ queue as a empty list for the next group of top 5 friends for each user/node
-        for id in queue: #repeats getting the top 5 for each user
-            friends_ids, followers_ids = get_friends_followers_ids(twitter_api, user_id=id, friends_limit=5000, followers_limit=5000)
-            response = popular(reciprocal_friends(friends_ids, followers_ids))
-            print("Top 5 Reciprocal Friends for id:",id,"is ", response)
-            for i in response:
-                if (i not in next_queue and i not in G.nodes()): next_queue.append(i)
-            for i in response:
-                G.add_edge(id, i)
-            # If the number of nodes exceeds 100, then break
-            if (G.number_of_nodes() >= 100):
-                collectedHundred = True
-                break
-
-    # print("Number of Nodes = {0}".format(nx.number_of_nodes(G)))
-    # #Collect the nodes and draw the graph using subgraphs
-    # connected_component_subgraphs = (G.subgraph(c) for c in nx.connected_components(G))
-    # sg = max(connected_component_subgraphs, key=len)
-    # print("The diameter is {0}".format(nx.diameter(sg)))
-    # print("The average distance is {0}".format(nx.average_shortest_path_length(sg)))
-    # # Draw the graph using matplot
-    # nx.draw(G, node_color='cyan', with_labels=True)
-    # plt.savefig("graph.png")
-    # plt.show()
-
-
-# Sample usage
-
-#crawl_followers(twitter_api, screen_name='edmundyu1001')
-
-# // KAREN PROJECT STARTS HERE \\ 
-
-# filter keywords
-# twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
-# q = 'healthcare worker'
-#q= EndLockdownsNow
-#stream = twitter_stream.statuses.filter(track=q, language='en')
-
-# q = 'NoMask'
-# results = twitter_api.search.tweets(q=q,count=100, geocode='40.789100,-73.135000,1000mi')['statuses']
-
-
-# for tweet in results:
-# #     # Filter out RTs
-#     if (not tweet['retweeted'] and 'RT @' not in tweet['text']):
-#         try:
-#             print()
-#             print("//////////////////////////////////////////NEXT USER/////////////////////////////////")
-#             print()
-
-#             print (tweet['id'], tweet['created_at'], tweet['text'])
-#         except:
-#             pass
+#Function taken from TwitterCookbook
 def harvest_user_timeline(twitter_api, screen_name=None, user_id=None, max_results=1000):
      
     assert (screen_name != None) != (user_id != None),     "Must have screen_name or user_id, but not both"    
@@ -303,7 +119,7 @@ def harvest_user_timeline(twitter_api, screen_name=None, user_id=None, max_resul
         
     results += tweets
     
-    print('Fetched {0} tweets'.format(len(tweets)), file=sys.stderr)
+   # print('Fetched {0} tweets'.format(len(tweets)), file=sys.stderr)
     
     page_num = 1
     
@@ -332,107 +148,81 @@ def harvest_user_timeline(twitter_api, screen_name=None, user_id=None, max_resul
         tweets = make_twitter_request(twitter_api.statuses.user_timeline, **kw)
         results += tweets
 
-        print('Fetched {0} tweets'.format(len(tweets)),file=sys.stderr)
+    #    print('Fetched {0} tweets'.format(len(tweets)),file=sys.stderr)
     
         page_num += 1
         
     print('Done fetching tweets', file=sys.stderr)
 
     return results[:max_results]
-
-# q = "Karen"
-# results = twitter_api.search.tweets(q=q,count=1000, geocode='34.1005166,-118.4146299,500mi')['statuses']
-# tweets = [r['user'] for r in results]
-# listOfNames = []
-
-# def condition(dic):
-#         namer = dic["name"]            
-#         karen = "Karen"
-#         if namer.find(karen) != -1 and (namer not in listOfNames):           
-#             return True
-            
-            
-# for i,t in enumerate(tweets):
-#     try:
-#         namer = t["name"] 
-#         if (condition(t) and (namer not in listOfNames)):
-#             listOfNames.append(namer)
-#             karenTweets = harvest_user_timeline(twitter_api, screen_name=t["screen_name"], max_results=100)
-#             print("//////////////////////////////////////////NEXT USER/////////////////////////////////")
-#             print(t["screen_name"])           
-#             for i, t in enumerate(karenTweets):
-#                 if (('vaccine' or 'mask') in t['text']):
-#                    # if (not t['retweeted'] and 'RT @' not in t['text']):
-#                         try:
-#                 #        print(i, "Timestamp: ", t['created_at'])
     
-#                                 print("\n")
-#                                 print(i, t['text']) # This 
-#                         except:
-#                                 pass
-#     except:
-#         pass
-# Megan, Sophia, Emma, Emily, Martha, Natalie
-from transformers import pipeline
-# make sure to include "from transformers import pipeline" at top of program
-userName= "Emma"
-sys.stdout = open('emma_tweets.txt', 'w') #printing output to text file
-sys.stdout.close() #This makes a clean text file each time its run so save the data if you need it later
 
-#getting users with Karen in their twitter handle/name
+#Start of Script
+
+# Looking for Twitter users that have that name
+userName= "Mary" 
+# This prints out tweets and tweet statistics to a text file 
+sys.stdout = open('tweets.txt', 'w') 
+# This makes a clean text file each time its run so save the data if you need it later
+sys.stdout.close() 
+
+# Getting users with userName in their twitter handle/name
+# Each iteration of results gets the Maximum number of 20 users, hence why we call it 5 times
 results = twitter_api.users.search(q=userName, page=1)
 results2 = twitter_api.users.search(q=userName, page=2)
 results3 = twitter_api.users.search(q=userName, page=3)
 results4 = twitter_api.users.search(q=userName, page=4)
 results5 = twitter_api.users.search(q=userName, page=5)
 
-#getting the Karen's names and locations
+# From the accounts that we just mined, we grab their screen_name and location 
 tweets1 = [(r['screen_name'], r['location']) for r in results]
 tweets2 = [(r['screen_name'], r['location']) for r in results2]
 tweets3 = [(r['screen_name'], r['location']) for r in results3]
 tweets4 = [(r['screen_name'], r['location']) for r in results4]
 tweets5 = [(r['screen_name'], r['location']) for r in results5]
 
-#combining into one list
+# Combining this information into one giant List
 tweets1.extend(tweets2)
 tweets1.extend(tweets3)
 tweets1.extend(tweets4)
 tweets1.extend(tweets5)
+
+# These variables will track total tweets, positive and negative tweets, as well as the confidence score
 tweetCount = 0
 posCount = 0
 negCount = 0
 confidenceScore = 0
+
 sentimentanalyzer = pipeline("sentiment-analysis")
 
 for (name,location) in tweets1:
-            karenTweets = harvest_user_timeline(twitter_api, screen_name=name, max_results=100)
-            sys.stdout = open('emma_tweets.txt', 'a')
+            karenTweets = harvest_user_timeline(twitter_api, screen_name=name, max_results=500) #Gathering the 500 most recent tweets from the given screen_name
+            sys.stdout = open('tweets.txt', 'a') #appending information to the text file
             print("//////////////////////////////////////////NEXT USER/////////////////////////////////")
             print(name) 
-            print(location)          
-            for i, t in enumerate(karenTweets):
-                #Do Megan Police tweets again
-                #if ('police' in t['text'] or 'BLM' in t['text'] ):
-                    # if (not t['retweeted'] and 'RT @' not in t['text']):
-                try:  
-                    tweetCount = tweetCount + 1
-                    d = sentimentanalyzer(t['text'])
-                    tweetLabel = d[0].get('label')
-                    tweetScore = d[0].get('score')
-                    if (tweetLabel=='POSITIVE'):
-                        posCount = posCount + 1
-                    else:
-                        negCount = negCount + 1
-                    confidenceScore = confidenceScore + float(tweetScore)
-                    print("\n")                                
-                    print(i, t['text'])
-                    print(d)
-                except:
-                    pass
+            print(location.encode('utf8'))          
+            for i, t in enumerate(karenTweets): # Parsing through the user's tweets
+                if ('Trump' in t['text'] or 'Biden' in t['text']): # This is where we insert keywords to filter the tweets through, in this case for out Presidents category
+                    # if (not t['retweeted'] and 'RT @' not in t['text']): # We had this line for when we wanted to see only original tweets
+                        try:  
+                                tweetCount = tweetCount + 1 
+                                d = sentimentanalyzer(t['text']) # Transformers package performing sentiment analysis on the tweet
+                                tweetLabel = d[0].get('label') # Gets the label
+                                tweetScore = d[0].get('score') # Gets the confidence score
+                                if (tweetLabel=='POSITIVE'):
+                                    posCount = posCount + 1
+                                else:
+                                    negCount = negCount + 1
+                                confidenceScore = confidenceScore + float(tweetScore)
+                                print("\n")                                
+                                print(i, t['text'].encode('utf8')) # prints out the tweet
+                                print(d) # prints out the label and confidence score
+                        except:
+                                pass
         
 
-averageScore = confidenceScore / float(tweetCount)
-positiveScore = (posCount / tweetCount) * 100
+averageScore = confidenceScore / float(tweetCount) # calculates average confidence score for all tweets from harvested users
+positiveScore = (posCount / tweetCount) * 100 # calculates the percentage of tweets that were positive
 print(f"This program read {tweetCount} tweets")
 print(f"Number of Positive Tweets is:{posCount}")
 print(f"Number of Number of Tweets is:{negCount}")
